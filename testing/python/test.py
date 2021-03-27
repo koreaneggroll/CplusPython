8a65d8b3e1928414106a05e8ca9cdbe7033248a5
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-03-27 20:31:29 +0200

    more efficient made some code more efficient

---
+++ testing/python/test.py
@@ -0,0 +1,13 @@
+hello = "Hello "
+
+
+print(hello.join("World"))
+print(hello.capitalize())
+print(hello.casefold())
+print(hello.count('o'))
+print(hello.endswith('o'))
+print(hello.startswith('H'))
+print(hello.find('l'))
+print(hello.index('l'))
+print(hello.replace('l', 'o'))
+print(hello.upper())
